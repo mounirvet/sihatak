ed8ad28980fe3336932eccae2259f6786e66cc34
mounirvet <49093244+mounirvet@users.noreply.github.com>
2026-06-10 16:21:37 +0400

    Add /adawat/ categories + products + al-ifsah to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,12 +2,13 @@ import { SITE, PILLARS } from '../lib/site';
 import { getArticleSlugs } from '../lib/content';
 import { getGlossarySlugs } from '../lib/glossary';
 import { getInsightSlugs } from '../lib/insights';
+import { getToolSlugs, getProductSlugs } from '../lib/tools';
 
 export const dynamic = 'force-static';
 
 export default function sitemap() {
   const now = new Date();
-  const staticPages = ['', '/mahawir', '/maqalat', '/jadeed', '/mustalahat', '/bahth', '/man-nahnu', '/man-nahnu/al-fariq-al-tibbi', '/man-nahnu/siyasat-al-tahrir', '/man-nahnu/al-masadir', '/man-nahnu/siyasat-al-khususiyya', '/man-nahnu/shurut-al-istikhdam', '/man-nahnu/ittasil-bina', '/man-nahnu/ikhla-al-masuliyya'];
+  const staticPages = ['', '/mahawir', '/maqalat', '/jadeed', '/mustalahat', '/bahth', '/adawat', '/man-nahnu', '/man-nahnu/al-fariq-al-tibbi', '/man-nahnu/siyasat-al-tahrir', '/man-nahnu/al-masadir', '/man-nahnu/siyasat-al-khususiyya', '/man-nahnu/shurut-al-istikhdam', '/man-nahnu/ittasil-bina', '/man-nahnu/ikhla-al-masuliyya', '/man-nahnu/al-ifsah'];
   const pages = staticPages.map((p) => ({
     url: `${SITE.url}${p}/`,
     lastModified: now,
@@ -28,5 +29,21 @@ export default function sitemap() {
     url: `${SITE.url}/jadeed/${slug}/`,
     lastModified: now,
   }));
-  return [...pages, ...pillarPages, ...articlePages, ...glossaryPages, ...insightPages];
+  const toolCategoryPages = getToolSlugs().map((slug) => ({
+    url: `${SITE.url}/adawat/${slug}/`,
+    lastModified: now,
+  }));
+  const productPages = getProductSlugs().map((slug) => ({
+    url: `${SITE.url}/adawat/muntaj/${slug}/`,
+    lastModified: now,
+  }));
+  return [
+    ...pages,
+    ...pillarPages,
+    ...articlePages,
+    ...glossaryPages,
+    ...insightPages,
+    ...toolCategoryPages,
+    ...productPages,
+  ];
 }
